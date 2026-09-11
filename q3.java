4a69af9e1adb4e287a875100c1b4534afb6273c3
Pilli Viswajith <viswajithpilli@gmail.com>
2026-09-11 23:16:17 +0530

    Implement threeSum method to find triplets summing to zero

---
+++ q3.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public int[][] threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<int[]> resultList = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    resultList.add(new int[]{nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return resultList.toArray(new int[resultList.size()][]);
+    }
+}
