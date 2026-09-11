06e6ef285d6cfb9e2adc672d3e785d69f517343b
Pilli Viswajith <viswajithpilli@gmail.com>
2026-09-11 23:33:34 +0530

    Implement player ranking and drafting logic

--- q5.java
+++ q5.java
@@ -22,3 +22,31 @@ public class Player implements Comparable<Player> {
     }
 
     @Override
+    public int compareTo(Player other) {
+        return Double.compare(other.battingAverage, this.battingAverage);
+    }
+
+    public static String draftAndRank(Player[] players) {
+        Player[] draftablePool = new Player[players.length];
+        int count = 0;
+        
+        for (Player p : players) {
+            if (isDraftable(p.matchesPlayed) || isDraftable(p.matchesPlayed, p.injured)) {
+                draftablePool[count++] = p;
+            }
+        }
+        
+        Player[] finalDraftable = Arrays.copyOf(draftablePool, count);
+        Arrays.sort(finalDraftable);
+        
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < finalDraftable.length; i++) {
+            result.append(i + 1).append(". ").append(finalDraftable[i].name);
+            if (i < finalDraftable.length - 1) {
+                result.append(" | ");
+            }
+        }
+        
+        return result.toString();
+    }
+}
