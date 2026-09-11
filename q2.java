2eba35c20fcaac74a882589670f91c1651c30dbf
Pilli Viswajith <viswajithpilli@gmail.com>
2026-09-11 23:25:53 +0530

    Create q2.java

---
+++ q2.java
@@ -0,0 +1,12 @@
+public class DuplicatePlayerPickChecker {
+    public static String findDuplicatePick(String[] playerNames) {
+        for (int i = 0; i < playerNames.length; i++) {
+            for (int j = i + 1; j < playerNames.length; j++) {
+                if (playerNames[i].equals(playerNames[j])) {
+                    return "Duplicate Found: " + playerNames[i];
+                }
+            }
+        }
+        return "No Duplicates Found";
+    }
+}
