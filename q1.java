7263c5264bc73c173cc185a640ec1d38ef150148
Pilli Viswajith <viswajithpilli@gmail.com>
2026-09-11 22:51:57 +0530

    Implement seat duplication checker class

---
+++ q1.java
@@ -0,0 +1,17 @@
+public class SeatDuplicationChecker {
+    public void checkDuplicateSeats(int[] seatNumbers) {
+        boolean found = false;
+        for (int i = 0; i < seatNumbers.length; i++) {
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+}
