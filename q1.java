503cdf926d299656f38c21b68da557fd47d31a66
Pilli Viswajith <viswajithpilli@gmail.com>
2026-09-11 23:03:34 +0530

    Add ATM PIN length validation

---
+++ q1.java
@@ -0,0 +1,9 @@
+public class AtmPinValidator {
+    public void checkPinLength(String pin) {
+        if (pin.length() == 4) {
+            System.out.println("PIN length OK.");
+        } else {
+            System.out.println("Invalid PIN — must be exactly 4 digits.");
+        }
+    }
+}
